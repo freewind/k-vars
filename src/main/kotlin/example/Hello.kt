326e229960769d106179baed54ab171d36620970
freewind <nowindlee@gmail.com>
2018-08-03 00:09:15 +0800

    demo

--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,7 +1,41 @@
 package example
 
 fun main(args: Array<String>) {
-    println(hello("Kotlin"))
+    val name = Var("kotlin")
+    val nameLength = name.map { it.length }
+
+    name.addListener { v -> println("newName: $v") }
+    nameLength.addListener { v -> println("newNameLength: $v") }
+
+    name.set("kotlin-changed")
 }
 
-fun hello(name: String): String = "Hello, $name!"
+typealias Listener<T> = (T) -> Unit
+
+class Var<T>(initValue: T) {
+    private var _value = initValue
+    private val listeners = mutableListOf<Listener<T>>()
+
+    fun addListener(listener: Listener<T>) {
+        this.listeners.add(listener)
+        listener(_value)
+    }
+
+    fun get() = _value
+
+    fun set(value: T) {
+        _value = value
+        listeners.forEach { it(value) }
+    }
+
+    fun <K> map(fn: (T) -> K): Var<K> {
+        val k = Var(fn(get()))
+        this.addListener { v ->
+            k.set(fn(v))
+        }
+        return k
+    }
+
+}
+
+
